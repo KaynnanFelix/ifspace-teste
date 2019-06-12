cee75f3464480bc08de825aa7b795f826e541277
CalebRees <reescaleb.cr@gmail.com>
2019-06-11 22:11:41 -0300

    Métodos de json completos

--- back-end/services/jsonMethods.php
+++ back-end/services/jsonMethods.php
@@ -1,45 +1,30 @@
-<table>
 <?php
 function getJson($file){
-//fetch data from json
 $data = file_get_contents($file);
-//decode into php array
 $data = json_decode($data);
-$index = 0;
-foreach($data as $row){
-    echo "
-
-        <tr>
-            <td>".$row->nome."</td>
-            <td>".$row->senha."</td>
-
-            <td>
-                <a href='edit.php?index=".$index."'>Edit</a>
-                <a href='delete.php?index=".$index."'>Delete</a>
-            </td>
-        </tr>
-    ";
-
-    $index++;
-}
+return $data;
 }
 function getIdJson($file,$id){
-
+$data=getJson($file);
+return $data[$id];
 }
-function addJson($file){
-
+function addJson($file,$obj){
+$data=getJson($file);
+$data[]=$data;
+saveJson($file,$data);
 }
-function editJson($file,$id){
-
+function editJson($file,$obj,$id){
+$data=getJson($file);
+$data[$id]=$obj;
+saveJson($file,$data);
 }
 function deleteJson($file,$id){
-    $data = file_get_contents($file);
-    $data = json_decode($data);
+    $data=getJson($file);
     unset($data[$id]);
+    saveJson($file,$data);
+}
+function saveJson($file,$data){
     $data=json_encode($data,JSON_PRETTY_PRINT);
     file_put_contents($file,$data);
 }
-getJson("../jsons/users/admin.json");
-deleteJson("../jsons/users/admin.json",1);
 ?>
-</table>
