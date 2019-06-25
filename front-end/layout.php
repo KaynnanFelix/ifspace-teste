8f4e0b17490b3848d33f0384f7acb43c629161a2
ClaudemirF <csacademico@outlook.com>
2019-06-24 21:30:49 -0300

    atualizando layout.php e laboratorios.json

--- front-end/layout.php
+++ front-end/layout.php
@@ -1,6 +1,6 @@
 <?php
 echo "<nav class='navbar navbar-expand-lg navbar-light bg-success'>
-<a class='navbar-brand' href='index.php'>IFSpace</a>
+<a class='navbar-brand'>IFSpace</a>
 <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
   <span class='navbar-toggler-icon'></span>
 </button>
