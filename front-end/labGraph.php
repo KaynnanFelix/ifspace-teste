bbb90b45610762879421f64e2acc04f280a113fc
CalebRees <reescaleb.cr@gmail.com>
2019-07-03 05:13:24 -0300

    Grafico de lab

---
+++ front-end/labGraph.php
@@ -0,0 +1,53 @@
+<?php
+include "../back-end/services/jsonMethods.php";
+$labs=getJson("../back-end/jsons/espacos/laboratorio/laboratorios.json");
+$registros=getJson("../back-end/jsons/espacos/laboratorio/registros.json");
+$registros=getJson("../back-end/jsons/espacos/registros.json");
+$dataPoints = array();
+
+foreach($labs as $lab){
+    $count=0;
+    foreach($registros as $registro){
+        if($lab->nome===$registro->nome){
+            $count++;
+        }
+    }
+    $dataPoints[]=array("label"=>$lab->nome, "y"=>$count);
+}
+print_r($dataPoints);
+
+
+	
+?>
+<!DOCTYPE HTML>
+<html>
+<head>  
+
+<script>
+window.onload = function () {
+ 
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	exportEnabled: true,
+	theme: "light1", // "light1", "light2", "dark1", "dark2"
+	title:{
+		text: "Simple Column Chart with Index Labels"
+	},
+	data: [{
+		type: "column", //change type to bar, line, area, pie, etc
+		//indexLabel: "{y}", //Shows y value on all Data Points
+		indexLabelFontColor: "#5A5757",
+		indexLabelPlacement: "outside",   
+		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+ 
+}
+</script>
+</head>
+<body>
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</body>
+</html>
