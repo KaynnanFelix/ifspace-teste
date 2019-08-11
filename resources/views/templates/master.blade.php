6f9d4d83d64090c53875cb52b7dffc4898959ceb
KaynnanFelix <kaynnanap.m.felix@gmail.com>
2019-08-11 14:44:02 -0300

    atualizandi favicon

--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-    <meta>
+        <meta charset="UTF-8">
+        <title>IFSpace - </title>
+        <meta name="author" content="Kaynnan Felix">
+        <meta name="description" content="">
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     </head>
     <body>
         
